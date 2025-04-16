b6e2195badbf6a76da00c9baf07875b7d652301e
CostaX549 <unitauchat@gmail.com>
2025-04-16 16:36:38 -0300

    Projeto TCC

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,12 @@
 <template>
-<section class="vh-100 bg-success" >
+<section class="vh-100 "  style="background-color: #77DD77;">
   <div class="container py-5 h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col col-xl-10">
         <div class="card" style="border-radius: 1rem;">
           <div class="row g-0">
             <div class="col-md-6 col-lg-5 d-none d-md-block">
-              <img src="~/assets/img/psicologia3.jpg"
+              <img src="~/assets/img/psicologa.jpg"
                 alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
             </div>
             <div class="col-md-6 col-lg-7 d-flex align-items-center">
@@ -32,11 +32,11 @@
                   </div>
 
                   <div class="pt-1 mb-4">
-                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-success btn-lg btn-block" type="button">Login</button>
+                    <button data-mdb-button-init data-mdb-ripple-init class="btn text-white  btn-lg btn-block"  style="background-color: #77DD77;" type="button">Login</button>
                   </div>
 
                   <a class="small text-muted" href="#!">Esqueceu a senha?</a>
-                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Não possui uma conta? <NuxtLink href="/register"
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Não possui uma conta? <NuxtLink href="/register"  style="color: #77DD77;"
                       >Registre-se aqui</NuxtLink></p>
                 
                 </form>
--- pages/register.vue
+++ pages/register.vue
@@ -1,7 +1,6 @@
 <template>
 
-
-<section class="vh-100 bg-success" >
+<section class="vh-100" style="background-color: #77DD77;">
 <ul class="nav nav-tabs nav-fill mb-3" id="ex1" role="tablist">
   <li class="nav-item" role="presentation">
     <a
@@ -40,52 +39,59 @@
     aria-labelledby="ex2-tab-1"
   >
   
-    <div class="container h-100 pt-5">
-      <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-          <div class="card" style="border-radius: 15px;">
-            <div class="card-body p-5">
-              <h2 class="text-uppercase text-center mb-5">Criar conta</h2>
-
-              <form>
-
-                <div data-mdb-input-init class="form-outline mb-4">
-                  <input type="text" id="form3Example1cg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example1cg">Nome</label>
-                </div>
-
-                <div data-mdb-input-init class="form-outline mb-4">
-                  <input type="email" id="form3Example3cg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example3cg">Email</label>
-                </div>
-
-                <div data-mdb-input-init class="form-outline mb-4">
-                  <input type="password" id="form3Example4cg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example4cg">Senha</label>
-                </div>
-
-                <div data-mdb-input-init class="form-outline mb-4">
-                  <input type="password" id="form3Example4cdg" class="form-control form-control-lg" />
-                  <label class="form-label" for="form3Example4cdg">Confirmar Senha</label>
-                </div>
-
-              
-                <div class="d-flex flex-column gap-2 justify-content-center">
-                  <button  type="button" data-mdb-button-init
-                    data-mdb-ripple-init class="btn btn-success btn-block btn-lg gradient-custom-4 text-white">Registrar</button>
-                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-lg btn-block btn-primary" style="background-color: #dd4b39;"
-                    type="submit"><i class="fab fa-google me-2"></i> Continuar com Google</button>
-                </div>
-              
-                <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!"
-                    class="fw-bold text-body"><u>Login here</u></a></p>
-
-              </form>
-
+    <div class="container h-100  pt-3">
+      <div class="row d-flex justify-content-center align-items-center ">
+      <div class="col col-xl-10">
+        <div class="card" style="border-radius: 1rem;">
+          <div class="row g-0">
+            <div class="col-md-6 col-lg-5 d-none d-md-block">
+              <img src="~/assets/img/psicologa.jpg"
+                alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
             </div>
-       
+            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+              <div class="card-body p-4 p-lg-5 text-black">
+
+                <form>
+
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                
+                    <span class="h1 fw-bold mb-0">Crie sua conta</span>
+                  </div>
+
+                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Faça registro da sua conta</h5>
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="text" id="form2Example17" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example17">Nome</label>
+                  </div>
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example17">Email</label>
+                  </div>
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example27">Senha</label>
+                  </div>
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example27">Confirmar Senha</label>
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <button data-mdb-button-init data-mdb-ripple-init class="btn text-white btn-lg btn-block"  type="button"  style="background-color: #77DD77;">Registrar</button>
+                  </div>
+
+
+                  <p class="mb-5 pb-lg-2" style="color: #393f81;">Já possui uma conta? <NuxtLink href="/login" style="color: #77DD77;"
+                      >Faça login aqui</NuxtLink></p>
+                
+                </form>
+
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+ 
     </div>
   </div>
   </div>
